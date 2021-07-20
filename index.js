5ac48bdf7749bd50b24583a250809ada481fd1fd
Matt Hunt <matthew-hunt@lambdastudents.com>
2021-07-19 19:27:59 -0700

    solve questions

--- index.js
+++ index.js
@@ -29,10 +29,16 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   
   1. What is the difference between counter1 and counter2?
   
+  Counter 1 is function scoped, and Counter 2 is global scoped.
+
   2. Which of the two uses a closure? How can you tell?
+
+  Counter1 uses a closure. You can tell by having the let count = 0 inside the function.
   
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+
+     Counter1 should be used preferably unless in a different scenario the count needs to be available on a global scope instead.
 */
 
 // counter1 code
@@ -62,9 +68,11 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+    return Math.floor(Math.random() * Math.floor(3));
 }
+
+console.log(`task 2`, inning());
 
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
@@ -81,19 +89,31 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(inningcb, numberOfInnings){
+  const score = { Away: 0, Home: 0};
+  for (let i = 0; i < numberOfInnings; i++){
+    score.Away = score.Away + inningcb()
+    score.Home += inningcb()
+  } return score;
 }
+ console.log(`task 3`, finalScore(inning, 1));
+ console.log(`task 3`, finalScore(inning, 2));
+ console.log(`task 3`, finalScore(inning, 3));
+ console.log(`task 3`, finalScore(inning, 4));
+ console.log(`task 3`, finalScore(inning, 5));
+ console.log(`task 3`, finalScore(inning, 6));
+ console.log(`task 3`, finalScore(inning, 7));
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inningcb) {
+  return { Away: inningcb(), Home: inningcb()};
 }
 
+console.log(`Task 4`, getInningScore(inning));
 
 /* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
 Use the scoreboard function below to do the following:
@@ -136,8 +156,10 @@ Use the scoreboard function below to do the following:
 ]  
   */
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function scoreboard(getInningcb, inningcb, numberOfInnings) {
+  array = []
+  homeScore = 0;
+  awayScore = 0;
 }
 
 
